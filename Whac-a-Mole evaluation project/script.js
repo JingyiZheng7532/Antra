851819b99035984824a29232394a2e5038a1b96d
Jingyi Zheng <jingyizheng6778@gmail.com>
2026-02-20 13:01:04 -0800

    Feat: timer

--- Whac-a-Mole evaluation project/script.js
+++ Whac-a-Mole evaluation project/script.js
@@ -1,7 +1,22 @@
+class HeaderView {
+  constructor() {
+    this.scoreLine = document.querySelector(".score-line");
+    this.beginBtn = document.querySelector(".begin-btn");
+  }
+  bindBtnHandler(handler) {
+    console.log("bind");
+    this.beginBtn.addEventListener("click", handler);
+  }
+}
+
+class HeaderMode {
+  #currScore;
+  constructor() {}
+}
+
 class GameBoardView {
   constructor(width, height) {
     this.boardElement = document.querySelector(".game-board");
-    console.log(this.boardElement);
     this.width = width;
     this.height = height;
   }
@@ -24,3 +39,76 @@ class GameBoardView {
 
 const gameBoardView = new GameBoardView(4, 3);
 gameBoardView.initRender();
+
+class TimerView {
+  constructor(timerModel) {
+    this.timerModel = timerModel;
+    this.timer = document.querySelector(".current-time");
+  }
+
+  timerRender() {
+    const currentTime = this.timerModel.currentRemaining;
+    this.timer.textContent = currentTime;
+  }
+}
+
+class TimerModel {
+  #currentRemaining;
+  constructor() {
+    this.#currentRemaining = 30;
+  }
+  set currentRemaining(value) {
+    this.#currentRemaining = value;
+  }
+
+  get currentRemaining() {
+    return this.#currentRemaining;
+  }
+
+  // begin() {
+  //   const time = setInterval(() => {
+  //     this.#currentRemaining--;
+
+  //     if (this.remainingTime <= 0) {
+  //       clearInterval(time);
+  //     }
+  //   }, 1000);
+  // }
+}
+
+class TimerController {
+  constructor(timerModel, timerView, headerView) {
+    this.timerModel = timerModel;
+    this.timerView = timerView;
+    this.headerView = headerView;
+
+    this.timerView.timerRender();
+
+    this.initEvents();
+  }
+
+  initEvents() {
+    this.headerView.bindBtnHandler(() => this.startGame());
+  }
+
+  startGame() {
+    console.log("start");
+    this.timerModel.currentRemaining = 30;
+    this.timerView.timerRender();
+    const time = setInterval(() => {
+      let currentRemaining = this.timerModel.currentRemaining;
+      currentRemaining--;
+      this.timerModel.currentRemaining = currentRemaining;
+      this.timerView.timerRender();
+
+      if (currentRemaining <= 0) {
+        clearInterval(time);
+      }
+    }, 1000);
+  }
+}
+
+const headerView = new HeaderView();
+const timerModel = new TimerModel();
+const timerView = new TimerView(timerModel);
+const timerController = new TimerController(timerModel, timerView, headerView);
